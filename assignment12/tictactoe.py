7519fd2a3b5ae32ff18affb344d35ba6162059e1
Olesia Mironenko <olesia.a.mironenko@gmail.com>
2025-06-02 02:30:26 -0400

    Task 6, bugs fixed

--- assignment12/tictactoe.py
+++ assignment12/tictactoe.py
@@ -36,16 +36,19 @@ class Board:
     def move(self, move_string):
         if not move_string in Board.valid_moves:
             raise TictactoeException("That's not a valid move.")
-        move_index = Board.valid_moves.index(move_string)
-        row = move_index // 3 # row
-        column = move_index % 3 #column
-        if self.board_array[row][column] != " ":
+        
+        # move_index = Board.valid_moves.index(move_string)
+        # row = move_index // 3 # row
+        # column = move_index % 3 #column
+        # if self.board_array[row][column] != " ":
+        #     raise TictactoeException("That spot is taken.")
+        
+        row, col = Board.move_to_coords[move_string]
+        if self.board_array[row][col] != " ":
             raise TictactoeException("That spot is taken.")
-        self.board_array[row][column] = self.turn
-        if self.turn == "X":
-            self.turn = "O"
-        else:
-            self.turn = "X"
+
+        self.board_array[row][col] = self.turn
+        self.turn = "O" if self.turn == "X" else "X"
 
     # 6.6: Add a whats_next() method
     def whats_next(self):
@@ -63,20 +66,15 @@ class Board:
                         break
         if not win:
             if self.board_array[1][1] != " ": # check diagonals
-                if self.board_array[0][0] == self.board_array[1][1] and self.board_array[2][2] == self.board_array[1][1]:
+                if self.board_array[0][0] == self.board_array[1][1] and self.board_array[1][1] == self.board_array[2][2]:
                     win = True
-                if self.board_array[0][2] ==  self.board_array[1][1] and self.board_array[2][0] == self.board_array[1][1]:
+                if self.board_array[0][2] ==  self.board_array[1][1] and self.board_array[1][1] == self.board_array[2][0]:
                     win = True
         if not win:
-            for i in range(3):
-                for j in range(3):
-                    if self.board_array[i][j] == " ":
-                        cat = False
-                    else:
-                        cat = True
-                    break
-            if (cat):
-                return (True, "Cat's Game.")
+            cat = False
+            if all(cell != " " for row in self.board_array for cell in row):
+                cat = True
+                return True, "Cat's Game"
 
         if not win and not cat:
             if self.turn == "X": 
